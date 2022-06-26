328e353a5c51ba74679c1b914423356b4c566ddf
s-bhatla <sidroot02@gmail.com>
2022-06-26 23:50:29 +0530

    added margin to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className='shadow-md w-full fixed top-0 left-0 mb-12'>
       <div className='md:flex items-center justify-between bg-black py-4 md:px-10 px-7'>
         <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-gray-800'>
